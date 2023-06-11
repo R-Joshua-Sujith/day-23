59f9b0666a7f58d8126f4a6595cc69c3e0f382f9
R-Joshua-Sujith <joshuasujith14@gmail.com>
2023-06-11 15:36:29 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container'>
+      <div className='card'>
+        <div className='offer'>FREE</div>
+        <div className='price'>$0<span>/month</span></div>
+        <hr></hr>
+        <div>
+          <p className='tick'>&#x2714; Single User</p>
+          <p className='tick'>&#x2714; 5GB Storage</p>
+          <p className='tick'>&#x2714; Unlimited Public Projects</p>
+          <p className='tick'>&#x2714; Community Access</p>
+          <p className='close'>&#x2717; Unlimited Private Projects</p>
+          <p className='close'>&#x2717; Dedicated Phone Support</p>
+          <p className='close'>&#x2717; Free Subdomain</p>
+          <p className='close'>&#x2717; Monthly Status Reports</p>
+        </div>
+        <button>BUTTON</button>
+      </div>
+
+      <div className='card'>
+        <div className='offer'>PLUS</div>
+        <div className='price'>$9<span>/month</span></div>
+        <hr></hr>
+        <div>
+          <p className='tick'>&#x2714;<span className='bold'>5 Users</span></p>
+          <p className='tick'>&#x2714; 50GB Storage</p>
+          <p className='tick'>&#x2714; Unlimited Public Projects</p>
+          <p className='tick'>&#x2714; Community Access</p>
+          <p className='tick'>&#x2714; Unlimited Private Projects</p>
+          <p className='tick'>&#x2717; Dedicated Phone Support</p>
+          <p className='tick'>&#x2717; Free Subdomain</p>
+          <p className='close'>&#x2717; Monthly Status Reports</p>
+        </div>
+        <button>BUTTON</button>
+      </div>
+
+      <div className='card'>
+        <div className='offer'>PRO</div>
+        <div className='price'>$49<span>/month</span></div>
+        <hr></hr>
+        <div>
+          <p className='tick'>&#x2714;<span className='bold'> Unlimited Users</span></p>
+          <p className='tick'>&#x2714; 150GB Storage</p>
+          <p className='tick'>&#x2714; Unlimited Public Projects</p>
+          <p className='tick'>&#x2714; Community Access</p>
+          <p className='tick'>&#x2714; Unlimited Private Projects</p>
+          <p className='tick'>&#x2714; Dedicated Phone Support</p>
+          <p className='tick'>&#x2714; <span className='bold'>Unlimited</span> Free Subdomain</p>
+          <p className='tick'>&#x2714; Monthly Status Reports</p>
+        </div>
+        <button>BUTTON</button>
+      </div>
+
+
+    </div >
   );
 }
 
